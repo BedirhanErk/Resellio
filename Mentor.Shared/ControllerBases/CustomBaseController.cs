0eff4a6ef18cd2f3e050b9e0932a0b676d973685
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-05-25 12:03:16 +0300

    IdentityServer added.

---
+++ Mentor.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Mentor.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mentor.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
