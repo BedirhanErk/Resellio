db7812ffdb863aeb9d3fa154e0f97de3fe41d36e
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-05-25 14:06:07 +0300

    Signup Endpoint added.

---
+++ IdentityServer/Mentor.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Mentor.IdentityServer.Dtos;
+using Mentor.IdentityServer.Models;
+using Mentor.Shared.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mentor.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City               
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(), 400));
+
+            return NoContent();
+        }
+    }
+}
