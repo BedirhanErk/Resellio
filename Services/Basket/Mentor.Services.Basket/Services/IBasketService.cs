382616240e7833dfaa1a86afe2be1155738ba963
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-05-28 12:48:02 +0300

    IBasketService added.

---
+++ Services/Basket/Mentor.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using Mentor.Services.Basket.Dtos;
+using Mentor.Shared.Dtos;
+
+namespace Mentor.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
