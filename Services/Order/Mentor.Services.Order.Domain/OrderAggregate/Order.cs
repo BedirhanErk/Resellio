d6d6054e6941d63c857442ac80b08b8226dbe310
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-05-30 16:09:30 +0300

    OrderAggregate created.

---
+++ Services/Order/Mentor.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,35 @@
+﻿using Mentor.Services.Order.Domain.Core;
+
+namespace Mentor.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Address address) 
+        {
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+            _orderItems = new List<OrderItem>();
+        }
+
+        public void AddOrderItem(string courseId, string courseName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(x => x.CourseId == courseId);
+
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(courseId, courseName, price, pictureUrl);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+}
---
+++ Services/Order/Mentor.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using Mentor.Services.Order.Domain.Core;
+
+namespace Mentor.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public string CourseId { get; private set; }
+        public string CourseName { get; private set; }
+        public decimal Price { get; private set; }
+        public string PictureUrl { get; private set; }
+
+        public OrderItem(string courseId, string courseName, decimal price, string pictureUrl)
+        {
+            CourseId = courseId;
+            CourseName = courseName;
+            Price = price;
+            PictureUrl = pictureUrl;
+        }
+
+        public void UpdateOrderItem(string courseName, decimal price, string pictureUrl)
+        {
+            CourseName = courseName;
+            Price = price;
+            PictureUrl = pictureUrl;
+        }
+    }
+}
