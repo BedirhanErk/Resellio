667b0ed2cfe121f60aa2f88e6c4263736c6d9175
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-05-31 11:30:20 +0300

    Order API: AutoMapper added.

---
+++ Services/Order/Mentor.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mentor.Services.Order.Application.Dtos;
+
+namespace Mentor.Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.Address, AddressDto>().ReverseMap();
+        }
+    }
+}
