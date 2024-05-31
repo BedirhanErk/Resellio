b2596133a92897f9064b73ddb7f570c404f3bf7b
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-05-31 11:14:54 +0300

    Order API: Dtos added.

---
+++ Services/Order/Mentor.Services.Order.Application/Dtos/CreatedOrderDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Mentor.Services.Order.Application.Dtos
+{
+    public class CreatedOrderDto
+    {
+        public int OrderId { get; set; }
+    }
+}
---
+++ Services/Order/Mentor.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mentor.Services.Order.Application.Dtos
+{
+    public class OrderItemDto
+    {
+        public string CourseId { get; private set; }
+        public string CourseName { get; private set; }
+        public decimal Price { get; private set; }
+        public string PictureUrl { get; private set; }
+    }
+}
