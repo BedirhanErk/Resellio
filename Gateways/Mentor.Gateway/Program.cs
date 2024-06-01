7aeaf5429329f0854a0f1cacd378a7aa680eba20
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-01 11:37:28 +0300

    Gateway: Ocelot Library added.

--- Gateways/Mentor.Gateway/Program.cs
+++ Gateways/Mentor.Gateway/Program.cs
@@ -1,5 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
+
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
