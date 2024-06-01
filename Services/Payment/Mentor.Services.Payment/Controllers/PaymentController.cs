fcbe327fe9bcaec11f44b06e435c383e19c0fcd9
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-01 10:48:57 +0300

    PaymentController added.

---
+++ Services/Payment/Mentor.Services.Payment/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Mentor.Shared.ControllerBases;
+using Mentor.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mentor.Services.Payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
