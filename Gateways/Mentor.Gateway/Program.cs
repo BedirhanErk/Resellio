87a4f232b8fcff2b83183bc6710013ff25cfc6c2
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-01 13:16:49 +0300

    Gateway: configuration.development and configuration.production created.

--- Gateways/Mentor.Gateway/Program.cs
+++ Gateways/Mentor.Gateway/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOcelot();
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName}.json")
+    .AddEnvironmentVariables();
+
 var app = builder.Build();
 
 await app.UseOcelot();
