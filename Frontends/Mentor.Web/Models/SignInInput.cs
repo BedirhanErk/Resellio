451299086285ce30153a2d9d8f356bda3accbce8
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-02 12:23:49 +0300

    IIdentityService added.

---
+++ Frontends/Mentor.Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mentor.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }   
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/Mentor.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModel.Client;
+using Mentor.Shared.Dtos;
+using Mentor.Web.Models;
+
+namespace Mentor.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SignInInput signInInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
