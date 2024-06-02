9ceac76820b667bb25532c573333c7fa18260ac2
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-02 14:29:20 +0300

    IdentityService SignIn completed.

---
+++ Frontends/Mentor.Web/Services/IdentityService.cs
@@ -0,0 +1,97 @@
+﻿using IdentityModel.Client;
+using Mentor.Shared.Dtos;
+using Mentor.Web.Models;
+using Mentor.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Globalization;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Mentor.Web.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClientSettings _clientSettings;
+        private readonly ServiceApiSettings _serviceApiSettings;
+
+        public IdentityService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IOptions<ClientSettings> clientSettings, IOptions<ServiceApiSettings> serviceApiSettings)
+        {
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+            _clientSettings = clientSettings.Value;
+            _serviceApiSettings = serviceApiSettings.Value;
+        }
+
+        public async Task<Response<bool>> SignIn(SignInInput signInInput)
+        {
+            var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.BaseUri,
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            });
+
+            if (disco.IsError)
+                throw disco.Exception;
+
+            var passwordTokenRequest = new PasswordTokenRequest();
+            passwordTokenRequest.ClientId = _clientSettings.WebClientForUser.ClientId;
+            passwordTokenRequest.ClientSecret = _clientSettings.WebClientForUser.ClientSecret;
+            passwordTokenRequest.UserName = signInInput.Email;
+            passwordTokenRequest.Password = signInInput.Password;
+            passwordTokenRequest.Address = disco.TokenEndpoint;
+
+            var token = await _httpClient.RequestPasswordTokenAsync(passwordTokenRequest);
+
+            if (token.IsError)
+            {
+                var responseContent = await token.HttpResponse.Content.ReadAsStringAsync();
+
+                var errorDto = JsonSerializer.Deserialize<ErrorDto>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+
+                return Response<bool>.Fail(errorDto.Errors, 404);
+            }
+
+            var userInfoRequest = new UserInfoRequest();
+            userInfoRequest.Token = token.AccessToken;
+            userInfoRequest.Address = disco.UserInfoEndpoint;
+
+            var userInfo = await _httpClient.GetUserInfoAsync(userInfoRequest);
+
+            if (userInfo.IsError)
+                throw userInfo.Exception;
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(userInfo.Claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", "role");
+
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var authenticationProperties = new AuthenticationProperties();
+            authenticationProperties.StoreTokens(new List<AuthenticationToken>()
+            {
+                new AuthenticationToken { Name = OpenIdConnectParameterNames.AccessToken, Value = token.AccessToken },
+                new AuthenticationToken { Name = OpenIdConnectParameterNames.RefreshToken, Value = token.RefreshToken },
+                new AuthenticationToken { Name = OpenIdConnectParameterNames.ExpiresIn, Value = DateTime.Now.AddSeconds(token.ExpiresIn).ToString("o", CultureInfo.InvariantCulture) }
+            });
+
+            authenticationProperties.IsPersistent = signInInput.IsRemember;
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authenticationProperties);
+
+            return Response<bool>.Success(200);
+        }
+
+        public Task<TokenResponse> GetAccessTokenByRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mentor.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Mentor.Shared.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; set; }
+    }
+}
