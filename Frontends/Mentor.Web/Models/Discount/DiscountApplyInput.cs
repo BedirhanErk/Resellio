f568e825f0a7d94366e4d2615859dac6764c0f99
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-05 22:06:47 +0300

    DiscountViewModel, DiscountApplyInput and DiscountApplyInputValidator created.

---
+++ Frontends/Mentor.Web/Models/Discount/DiscountApplyInput.cs
@@ -0,0 +1,7 @@
+﻿namespace Mentor.Web.Models.Discount
+{
+    public class DiscountApplyInput
+    {
+        public string Code { get; set; }
+    }
+}
---
+++ Frontends/Mentor.Web/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Mentor.Web.Models.Discount;
+
+namespace Mentor.Web.Validators
+{
+    public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+    {
+        public DiscountApplyInputValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty().WithMessage("Discount code is required.");
+        }
+    }
+}
