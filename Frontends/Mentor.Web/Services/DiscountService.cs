ac1d2ba0ae31d8910d0a24e64314a1db0accf1c8
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-05 22:22:20 +0300

    DiscountService/GetDiscount completed.

--- Frontends/Mentor.Web/Services/DiscountService.cs
+++ Frontends/Mentor.Web/Services/DiscountService.cs
@@ -1,13 +1,28 @@
-﻿using Mentor.Web.Models.Discount;
+﻿using Mentor.Shared.Dtos;
+using Mentor.Web.Models.Discount;
 using Mentor.Web.Services.Interfaces;
 
 namespace Mentor.Web.Services
 {
     public class DiscountService : IDiscountService
     {
-        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        private readonly HttpClient _httpClient;
+
+        public DiscountService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            var response = await _httpClient.GetAsync($"discount/GetByCode/{discountCode}");
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var responseSuccess = await response.Content.ReadFromJsonAsync<Response<DiscountViewModel>>();
+
+            return responseSuccess.Data;
         }
     }
 }
