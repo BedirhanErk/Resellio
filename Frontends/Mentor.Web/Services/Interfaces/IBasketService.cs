9b613018441e7a363877539c1e177542b36a43d4
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-05 13:32:04 +0300

    IBasketService added.

---
+++ Frontends/Mentor.Web/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,15 @@
+﻿using Mentor.Web.Models.Basket;
+
+namespace Mentor.Web.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<BasketViewModel> GetBasket(string userId);
+        Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+        Task<bool> Delete(string userId);
+        Task AddBasketItem(BasketItemViewModel basketItemViewModel);
+        Task<bool> RemoveBasketItem(string courseId);
+        Task<bool> ApplyDiscount(string discountCode);
+        Task<bool> CancelApplyDiscount();
+    }
+}
