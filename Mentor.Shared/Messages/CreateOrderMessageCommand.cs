b5b64b2d951797bbe33073c8a53da0ddceae6fa6
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-06-07 18:13:00 +0300

    RabbitMQ -> MassTransit:  CreateOrderMessageCommand created.

---
+++ Mentor.Shared/Messages/CreateOrderMessageCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Mentor.Shared.Messages
+{
+    public class CreateOrderMessageCommand
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Line { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
