56e6896f4ae9e0a16cbc7b7a000168f9a17411e8
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-07-10 15:50:18 +0300

    Checkout updated.

--- Frontends/Resellio.Web/Controllers/OrderController.cs
+++ Frontends/Resellio.Web/Controllers/OrderController.cs
@@ -27,6 +27,42 @@ namespace Resellio.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Checkout(CheckoutInfoInput checkoutInfoInput)
         {
+            if (checkoutInfoInput.Province == "" || checkoutInfoInput.Province == null)
+                ModelState.AddModelError("Province", "Province is required.");
+
+            if (checkoutInfoInput.District == "" || checkoutInfoInput.District == null)
+                ModelState.AddModelError("District", "District is required.");
+            
+            if (checkoutInfoInput.Street == "" || checkoutInfoInput.Street == null)
+                ModelState.AddModelError("Street", "Street is required.");
+
+            if (checkoutInfoInput.ZipCode == "" || checkoutInfoInput.ZipCode == null)
+                ModelState.AddModelError("ZipCode", "Zip Code is required.");
+
+            if (checkoutInfoInput.Line == "" || checkoutInfoInput.Line == null)
+                ModelState.AddModelError("Line", "Address is required.");
+
+            if (checkoutInfoInput.Name == "" || checkoutInfoInput.Name == null)
+                ModelState.AddModelError("Name", "Name on Card is required.");
+
+            if (checkoutInfoInput.CardNumber == "" || checkoutInfoInput.CardNumber == null)
+                ModelState.AddModelError("CardNumber", "Card Number is required.");
+
+            if (checkoutInfoInput.Expiration == "" || checkoutInfoInput.Expiration == null)
+                ModelState.AddModelError("Expiration", "Expiry Date is required.");
+
+            if (checkoutInfoInput.CVV == "" || checkoutInfoInput.CVV == null)
+                ModelState.AddModelError("CVV", "CVV is required.");
+
+            if (!ModelState.IsValid)
+            {
+                var basket = await _basketService.GetBasket();
+
+                ViewBag.basket = basket;
+
+                return View();
+            }
+
             var orderStatus = await _orderService.SuspendOrder(checkoutInfoInput);
 
             if (!orderStatus.IsSuccessful)
--- Frontends/Resellio.Web/Models/Order/CheckoutInfoInput.cs
+++ Frontends/Resellio.Web/Models/Order/CheckoutInfoInput.cs
@@ -10,22 +10,16 @@ namespace Resellio.Web.Models.Order
 
         public string Street { get; set; }
 
-        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
 
-        [Display(Name = "Address")]
         public string Line { get; set; }
 
-        [Display(Name = "Name on card")]
         public string Name { get; set; }
 
-        [Display(Name = "Card number")]
         public string CardNumber { get; set; }
 
-        [Display(Name = "Expiration date")]
         public string Expiration { get; set; }
 
-        [Display(Name = "Security code")]
         public string CVV { get; set; }
     }
 }
